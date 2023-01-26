d079237deea573118e0f6879fec1fb986ac72906
philip.interviews <philip.interviews@gmail.com>
2023-01-26 12:26:17 -0500

    update problem descriptions

--- Problems/BestTimeToBuySellStock/best_time_to_buy_sell_stock.go
+++ Problems/BestTimeToBuySellStock/best_time_to_buy_sell_stock.go
@@ -1,12 +1,10 @@
 /* 
-You are given an array prices where prices[i] is the price of a given stock on the ith day. 
-You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
-Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
-Example:
-prices = [7,1,5,3,6,4]
-Output: 5
-Explanation: If you buy on day 2 at 1 and sell on day 5 at 6, that's 6-1 = 5 in profit. That's the maximum profit you can make given the input data.
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: true
 */
 package main
 
